3d477d63e901b07de0511fabfacbb3b0904f124b
Kwangsoo Kim <gimgwangsu0@gmail.com>
2026-05-14 21:58:38 +0900

    feat: 에메일 전송 기능 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, MailModule],
   controllers: [AppController],
   providers: [AppService],
 })
